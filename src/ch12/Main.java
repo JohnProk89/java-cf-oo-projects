3368e61d9c07c7a88734798d52c989e6416efefb
Giannis Prokopiou <johnprok89@gmail.com>
2025-02-20 18:10:07 +0200

    ch12

---
+++ src/ch12/Main.java
@@ -0,0 +1,16 @@
+package ch12;
+
+import ch12.model.User;
+import ch12.model.UserCredentials;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        User john = new User(1l, "John", "Prokopiou");
+        UserCredentials johnp = new UserCredentials(1l, "johnprok", "12345");
+
+        System.out.println(john.getId() + ", " + john.getFirstname() + ", " + john.getLastname());
+        System.out.println(johnp.getId() + ", " + johnp.getUsername() + ", " + johnp.getPassword());
+    }
+}
