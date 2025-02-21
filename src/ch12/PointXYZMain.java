d9cc7e810a5a449408114bc920ae34fb006cc633
Giannis Prokopiou <johnprok89@gmail.com>
2025-02-21 17:26:54 +0200

    Doc comments

--- src/ch12/PointXYZMain.java
+++ src/ch12/PointXYZMain.java
@@ -1,2 +1,28 @@
-package ch12;public class PointXYZMain {
+package ch12;
+
+import ch12.model.PointXYZ;
+
+public class PointXYZMain {
+
+    public static void main(String[] args) {
+
+        PointXYZ point1 = new PointXYZ(1, 1, 1);
+        PointXYZ point2 = new PointXYZ(2, 2, 2);
+
+        System.out.println("I got the following points:");
+        System.out.println(point1.convertToString());
+        System.out.println(point2.convertToString());
+
+        double xyDistance = point1.getXYDistance(point2);
+        System.out.printf("Their distance in the xy plane is: %.4f \n", xyDistance);
+
+        double yzDistance = point1.getYZDistance(point2);
+        System.out.printf("Their distance in the yz plane is: %.4f \n", yzDistance);
+
+        double xzDistance = point1.getXZDistance(point2);
+        System.out.printf("Their distance in the xz plane is: %.4f \n", xzDistance);
+
+        double xyzDistance = point1.getXYZDistance(point2);
+        System.out.printf("Their distance in 3d is: %.4f\n", xyzDistance);
+    }
 }
--- src/ch12/model/PointXYZ.java
+++ src/ch12/model/PointXYZ.java
@@ -1,2 +1,72 @@
-package ch12.model;public class PointXYZ {
+package ch12.model;
+
+/**
+ * Class representing a three-dimensional point
+ */
+public class PointXYZ {
+    private int x;
+    private int y;
+    private int z;
+
+    public PointXYZ() {
+    }
+
+    public PointXYZ(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+    }
+
+    public String convertToString(){
+        return "(" + x + ", " + y + ", " + z + ")";
+    }
+
+    public double getXYDistance(PointXYZ point){
+        int dx = this.getX() - point.getX();
+        int dy = this.getY() - point.getY();
+        return Math.sqrt(Math.pow(dx,2) + Math.pow(dy,2));
+    }
+
+    public double getYZDistance(PointXYZ point){
+        int dy = this.getY() - point.getY();
+        int dz = this.getZ() - point.getZ();
+        return Math.sqrt(Math.pow(dy,2) + Math.pow(dz,2));
+    }
+
+    public double getXZDistance(PointXYZ point){
+        int dx = this.getX() - point.getX();
+        int dz = this.getZ() - point.getZ();
+        return Math.sqrt(Math.pow(dx,2) + Math.pow(dz,2));
+    }
+
+    public double getXYZDistance(PointXYZ point){
+        int dx = this.getX() - point.getX();
+        int dy = this.getY() - point.getY();
+        int dz = this.getZ() - point.getZ();
+        return Math.sqrt(Math.pow(dx,2) + Math.pow(dy,2) + Math.pow(dz,2));
+    }
 }
