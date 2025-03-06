76296b62fdd585425cc3ad34333e47b25b14fe64
Giannis Prokopiou <johnprok89@gmail.com>
2025-03-06 12:12:28 +0200

    l to L in long for readability

--- src/ch12/Main.java
+++ src/ch12/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        User john = new User(1l, "John", "Prokopiou");
+        User john = new User(1L, "John", "Prokopiou");
         UserCredentials johnp = new UserCredentials(1l, "johnprok", "12345");
 
         System.out.println(john.getId() + ", " + john.getFirstname() + ", " + john.getLastname());
