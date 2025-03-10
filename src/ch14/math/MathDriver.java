0a797ab2fb40250cb25b3feb9018a6f6e596e4a2
Giannis Prokopiou <johnprok89@gmail.com>
2025-03-10 15:14:32 +0200

    math exercise

--- src/ch14/math/MathDriver.java
+++ src/ch14/math/MathDriver.java
@@ -1,2 +1,13 @@
-package ch14.math;public class MathDriver {
+package ch14.math;
+
+public class MathDriver {
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int max = MathHelper.getMaxArrayValue(array);
+        int min = MathHelper.getMinArrayValue(array);
+        double avg = MathHelper.getAverage(array);
+
+        System.out.printf("Tha array's max value is %d and tha min value is %d and the average is %.2f\n", max, min, avg);
+    }
 }
--- src/ch14/math/MathHelper.java
+++ src/ch14/math/MathHelper.java
@@ -1,4 +1,50 @@
-package ch14;
+package ch14.math;
 
+/**
+ * Utility class on arrays
+ */
 public class MathHelper {
+
+    /**
+     * Gets the max value of an array
+     * @param array the input array
+     * @return the max value
+     */
+    public static int getMaxArrayValue(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    /**
+     * Gets the min value of an array
+     * @param array the input array
+     * @return the min value
+     */
+    public static int getMinArrayValue(int[] array) {
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    /**
+     * Calculated the average of all the array values
+     * @param array the input array
+     * @return the average
+     */
+    public static double getAverage(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return (double) sum / array.length;
+    }
 }
