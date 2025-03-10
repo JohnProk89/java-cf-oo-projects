352c5cca6703e8430c0e6a69fd8994cdbf8fcd44
Giannis Prokopiou <johnprok89@gmail.com>
2025-03-10 17:27:25 +0200

    validation exercise

--- src/ch14utilitiesfactories/validation/Driver.java
+++ src/ch14utilitiesfactories/validation/Driver.java
@@ -1,2 +1,15 @@
-package ch14utilitiesfactories.validation;public class Driver {
+package ch14utilitiesfactories.validation;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        String strGood = "I am good";
+        String strBad = "I am way out of limits bro, or maybe now I am indeed";
+        int numGood = 9;
+        int numBad = -4;
+        System.out.printf("String: '%s' is within limits: %b\n", strGood, ValidationUtils.isLengthOK(strGood));
+        System.out.printf("String: '%s' is within limits: %b\n", strBad, ValidationUtils.isLengthOK(strBad));
+        System.out.printf("Number: '%d' is within limits: %b\n", numGood, ValidationUtils.isNumberOK(numGood));
+        System.out.printf("Number: '%d' is within limits: %b\n", numBad, ValidationUtils.isNumberOK(numBad));
+    }
 }
--- src/ch14utilitiesfactories/validation/ValidationUtils.java
+++ src/ch14utilitiesfactories/validation/ValidationUtils.java
@@ -1,4 +1,25 @@
-package ch14utilitiesfactories;
+package ch14utilitiesfactories.validation;
 
+/**
+ * Validation utility class
+ */
 public class ValidationUtils {
+
+    /**
+     * Checks if a string has valid length
+     * @param str the input string
+     * @return true if string in limits
+     */
+    public static boolean isLengthOK(String str) {
+        return !str.isEmpty() && str.length() <= 31;
+    }
+
+    /**
+     * Checks if a number is in limits
+     * @param num the input number
+     * @return true if number in limits
+     */
+    public static boolean isNumberOK(int num) {
+        return num >= 1 && num <= 10;
+    }
 }
